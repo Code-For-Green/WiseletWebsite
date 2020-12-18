c94cd3376c00267c1ce04a32bda38bcbc9d462d7
Crawcik <crawcik@gmail.com>
2020-12-18 11:10:05 +0100

    Fixed version

--- src/add.php
+++ src/add.php
@@ -3,14 +3,14 @@
     $username = $_GET["user"];
     $password = $_GET["pass"];
     $dbname = "$249391552_0000017";
-
     $conn = new mysqli($servername, $username, $password, $dbname);
 
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-
-    $sql = "INSERT INTO dane (flow, DateTime) VALUES ('$_GET["flow"]', 'date()')";
+    $flow = $_GET["flow"];
+    $date_now = date('Y-m-d H:i:s');
+    $sql = "INSERT INTO dane(flow, dateTime) VALUES('$flow', '$date_now')";
 
     if ($conn->query($sql) === TRUE) {
         echo "New record created successfully";
