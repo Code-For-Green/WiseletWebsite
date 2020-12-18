40bc16bdbca9f68f6a7c2f99595f5a4fde5f7073
Crawcik <crawcik@gmail.com>
2020-12-18 09:40:17 +0100

    Code for arduino

--- src/add.php
+++ src/add.php
@@ -1,3 +1,22 @@
 <?php
+    $servername = "localhost";
+    $username = $_GET["user"];
+    $password = $_GET["pass"];
+    $dbname = "$249391552_0000017";
 
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "INSERT INTO dane (flow, DateTime) VALUES ('$_GET["flow"]', 'date()')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
 ?>
